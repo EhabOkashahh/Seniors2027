4707367d6096862e1b461403b7ae0d7947dedbde
Ehab <okashahehab@gmail.com>
2026-05-21 23:21:51 +0300

    Update Client

--- SeniorsSevrer/Seniors2027.API/Seniors2027.DAL/obj/Debug/net9.0/Seniors2027.DAL.AssemblyInfo.cs
+++ SeniorsSevrer/Seniors2027.API/Seniors2027.DAL/obj/Debug/net9.0/Seniors2027.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Seniors2027.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16e85350422fb1edd124f572c65b8bc88c28be60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf84ebf560e10607eca842f6b5490068b17da3d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Seniors2027.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Seniors2027.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
