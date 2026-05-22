cb72e7fbbf4fca5fcead6bd9116fdb44dc20fcef
Ehab <okashahehab@gmail.com>
2026-05-22 17:17:59 +0300

    Add Memory board

--- SeniorsSevrer/Seniors2027.API/Seniors2027.API/obj/Release/net9.0/Seniors2027.API.AssemblyInfo.cs
+++ SeniorsSevrer/Seniors2027.API/Seniors2027.API/obj/Release/net9.0/Seniors2027.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Seniors2027.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca3ef7ccb840d9065e44d839ba24730104b8cf1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9318c18365261f3bf519b05e4d8c5caf4a2ad74d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Seniors2027.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Seniors2027.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
